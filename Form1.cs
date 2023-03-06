f3a9a6388780bb7ce21a697c5cd6d17879eb991b
jaume <jmuste3@vidalibarraquer.net>
2023-03-06 12:14:18 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+using System.Windows.Forms;
+
+namespace DemoHelpSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Crea una nova instància del control HelpProvider
+            //HelpProvider helpProvider1 = new HelpProvider();
+            //(Afegir Control HelProvider al Form1)
+
+            //Per mostrar el botó d'ajuda contextual cal assignar a aquestes propietats:
+            this.HelpButton = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+
+            // Defineix el fitxer d'ajuda
+            //helpProvider1.HelpNamespace = "MyApp.htm";
+            //helpProvider1.HelpNamespace = "";
+            
+
+            // Associar el control HelpProvider amb cada control que té ajuda contextual
+            helpProvider1.SetHelpString(this.button1, "Aquest botó serveix per desar les dades");
+            helpProvider1.SetHelpString(this.textBox1, "Introduïu el vostre nom aquí");
+
+        }
+    }
+}
